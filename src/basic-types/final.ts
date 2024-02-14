731534c8f79f6f8cdec0d38464cd6e6c38c9857a
arisbousios <124040952+arisbousios@users.noreply.github.com>
2024-02-14 16:01:34 +0200

    basic types exercise (#8)
    basic types exercise

---
+++ src/basic-types/final.ts
@@ -0,0 +1,33 @@
+enum QuestionCategory {
+  GeneralKnowledge = 'GeneralKnowledge',
+  Science = 'Science',
+  Math = 'Math',
+  Programming = 'Programming',
+}
+
+type Question = {
+  category: QuestionCategory;
+  question: string;
+  correctAnswer: boolean;
+};
+
+const questions: Question[] = [
+  {
+    category: QuestionCategory.GeneralKnowledge,
+    question: 'The sun is a star.',
+    correctAnswer: true,
+  },
+  {
+    category: QuestionCategory.Science,
+    question: 'Water boils at 200 degrees Celsius.',
+    correctAnswer: false,
+  },
+];
+
+function checkAnswer(question: Question, userAnswer: boolean): boolean {
+  return question.correctAnswer === userAnswer;
+}
+
+checkAnswer(questions[0], false);
+
+console.log(`Your answer is:`, checkAnswer(questions[1], true));
