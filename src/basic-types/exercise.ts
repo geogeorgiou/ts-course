da1f132cf427aa96d74537c265b76ea5e1ddaada
Georgios Georgiou <georgios.georgiou@newcrosshealthcare.com>
2024-02-14 16:00:05 +0200

    remove exercise, add typecheck script

--- src/basic-types/exercise.ts
+++
@@ -1,6 +0,0 @@
-// Create a TypeScript program that models a simple quiz with true/false questions.
-// Use an array of questions objects "questions" to store: category, question, correctAnswer
-// the categories could be: GeneralKnowledge, Science, Math, Programming
-// then create a checkAnswer function that will accept a question and the user answer and will return if it is correct
-// use it like: checkAnswer(questions[0], false);
-// console.log(`Your answer is:`, checkAnswer(questions[1], true));
